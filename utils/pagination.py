a6734d89c7713e61fc9faf28cd4c6dc7f29f4e2f
rxg <ruanxiguang@foxmail.com>
2022-07-13 22:37:15 +0800

    fix: 修复用户分页功能

--- utils/pagination.py
+++ utils/pagination.py
@@ -3,11 +3,13 @@ from rest_framework.response import Response
 
 
 class PageNumberPagination(pagination.PageNumberPagination):
+    page_size_query_param = 'page_size'
+    max_page_size = 200
+
     def get_paginated_response(self, data):
         return Response({
             'pagination': {
                 'total': self.page.paginator.count,
-                'size': self.page_size,
                 'page': self.page.number
             },
             'results': data,
